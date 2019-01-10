f7c9faaa6279ac45b2c90386a8be1bd30d61267c
Hugo Cicarelli <hcicarelli@gmail.com>
2019-01-10 08:30:40 -0200

    Initial commit with Vantage Theme

---
+++ wp-content/themes/vantage-child/front-page.php
@@ -0,0 +1,14 @@
+<?php
+
+get_header();
+?>
+
+<div id="primary" class="content-area">
+	<div id="content" class="site-content" role="main">
+		<div class="entry-content">
+			teste
+		</div>
+	</div><!-- #content .site-content -->
+</div><!-- #primary .content-area -->
+
+<?php get_footer(); ?>
